eee61a112d58e0792d0938b3c01655aaa36d69bd
weizmm <weizmm05@gmail.com>
2018-11-04 09:42:45 +0200

    fix/define variable from config file

--- src/es/es_synchronizeData.js
+++ src/es/es_synchronizeData.js
@@ -2,6 +2,7 @@ const axios = require("axios");
 const saveAsFile = require('../util/saveAsFile');
 const dataComparison = require('../util/dataComparison');
 const p = require('../config/paths');
+const fn = require('../config/fieldNames');
 
 module.exports = async()=>{
     // get the update data from the remote server
@@ -9,7 +10,7 @@ module.exports = async()=>{
     // save the new json as file in the server
     let previous_es_data_file_name = saveAsFile(es_data.data,'./data/es','es_raw_data');
    // get the delta between the two last JSONs
-    esDiff = dataComparison(es_data.data,"./data/es/archive", previous_es_data_file_name, "tz");
+    esDiff = dataComparison(es_data.data,"./data/es/archive", previous_es_data_file_name, fn.es.identityCard);
     
     return esDiff;
 
