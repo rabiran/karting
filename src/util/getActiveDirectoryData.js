eff6f824103c911d7933fe9c0f9ff70e01d4c760
Daniel <danielezra10@gmail.com>
2018-11-04 20:16:23 +0200

    fixed ad, now getting from api. need to create mock still not finish!

--- src/util/getActiveDirectoryData.js
+++ src/util/getActiveDirectoryData.js
@@ -1,25 +1,10 @@
-const ActiveDirectory = require('activedirectory');
+const axios = require("axios");
+const p = require('../config/paths');
 const fn = require('../config/fieldNames');
-const config = {
-  url:fn.ldapURL,
-  username: fn.LDAP_USER,
-  password: fn.LDAP_PASSWORD
-}
-const ad = new ActiveDirectory(config)
 const rExpAD = /^t{1}[0-9]{1}[a-zA-Z\d]+$/i
 const rExpPN = /^t{2}\d{7}$/i
 const rExpUPN = /^s{1}\d{7}$/i
-const usersQuery="(&(objectCategory=person)(objectClass=user))"
-const ousQuery = "(ou>='')"
-const BASE_LDAP_DN = fn.baseDN;
-const ouOpts = {
-  filter: ousQuery,
-  scope: "one",
-  baseDN: BASE_LDAP_DN
-}
-
 let adUsers = []
-let ous = []
 
 function getPrincipalName(userId) {
   let mi
@@ -36,44 +21,10 @@ function getPrincipalName(userId) {
   return [mi, mail]
 }
 
-async function getAllADOus(query) {
-  return new Promise((resolve, reject) => {
-    ad.find(query, (err, ous) => {
-      if(err) {
-        reject(err)
-      } else {
-        resolve(ous.other)
-      }
-    })
-  })
-}
-
-async function getAllADUsers(query) {
-  return new Promise((resolve, reject) => {
-    ad.find(query, (err, users) => {
-      if(err) {
-        reject(err)
-      } else {
-        resolve(users)
-      }
-    })
-  })
-}
-
 module.export = async (user) => {
-    ous = await getAllADOus(ouOpts)
-    for(const ou of ous) {
-      let usersOpts ={
-        filter: usersQuery,
-        baseDn: ou.dn
-      }
-      users = await getAllADUsers(usersOpts)
-      if(users != null) {
-        adUsers = adUsers.concat(users)
-      }
-    }
     let UPN
-    let userId = user[uniqueId] ? user[uniqueId].split('@')[0] : ""
+    adUsers = await axios.get(p().AD_API);
+    let userId = user[fn.nv.uniqueId] ? user[fn.nv.uniqueId].split('@')[0] : ""
     userData = getPrincipalName(userId.toUpperCase())
     if(rExpAD.test(userId)) {
       UPN = userData[0]
@@ -81,11 +32,11 @@ module.export = async (user) => {
       user[mi] = rExpUPN.test(UPN) ? UPN.substr(1) : ""
     }
     else if(rExpPN.test(userId)) {
-        user[mi] =userId.subStr(2)
+        user[fn.aka.personalNumber] =userId.subStr(2)
     } else {
-        user[mi] =""
+        user[fn.aka.personalNumber] =""
     }
-    user[mail] = userData[1]
+    user[fn.aka.mail] = userData[1]
 
     return user;
 }
