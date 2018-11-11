2deba462dd0945137341dc446e796ff9489580d2
Daniel231 <Danielezra10@gmail.com>
2018-11-11 12:01:03 +0200

    Update getActiveDirectoryData.js

--- src/util/getActiveDirectoryData.js
+++ src/util/getActiveDirectoryData.js
@@ -7,36 +7,38 @@ const rExpUPN = /^s{1}\d{7}$/i
 let adUsers = []
 
 function getPrincipalName(userId) {
-  let mi
-  let mail
+  let data = null
   adUsers.forEach((adUser, index) => {
     if(adUser.sAMAccountName == userId) {
       mi = adUser.userPrincipalName != null ? adUser.userPrincipalName : ""
       mail = adUser.mail != null ? adUser.mail : ""
       adUser.splice(index, 1)
+      data = [mi , mail]
       return
     }
   })
   
-  return [mi, mail]
+  return data
 }
 
 module.export = async (user) => {
+    adUsers = await axios.get(p().AD_API)
+    adUsers = adUsers.data
     let UPN
-    adUsers = await axios.get(p().AD_API);
     let userId = user[fn.nv.uniqueId] ? user[fn.nv.uniqueId].split('@')[0] : ""
-    userData = getPrincipalName(userId.toUpperCase())
-    if(rExpAD.test(userId)) {
+    let userDomain = user[fn.nv.uniqueId] ? user[fn.nv.uniqueId].split('@')[1] : ""
+    if(rExpAD.test(userId) && userDomain == "bla" ) {
+      userData = getPrincipalName(userId.toUpperCase())
       UPN = userData[0]
       UPN = UPN.split('@')[0]
-      user[mi] = rExpUPN.test(UPN) ? UPN.substr(1) : ""
+      user[fn.aka.personalNumber] = rExpUPN.test(UPN) ? UPN.substr(1) : ""
+      user[fn.aka.mail] = userData[1]
     }
     else if(rExpPN.test(userId)) {
-        user[fn.aka.personalNumber] =userId.subStr(2)
+        user[fn.aka.personalNumber] =userId.substr(2)
     } else {
         user[fn.aka.personalNumber] =""
     }
-    user[fn.aka.mail] = userData[1]
 
     return user;
 }
