c95018fe837047ca52a446226a08ade6873d3a65
Daniel231 <Danielezra10@gmail.com>
2018-11-20 15:32:09 +0200

    Update getActiveDirectoryData.js

--- src/util/getActiveDirectoryData.js
+++ src/util/getActiveDirectoryData.js
@@ -1,6 +1,7 @@
 const axios = require("axios");
 const p = require('../config/paths');
 const fn = require('../config/fieldNames');
+const logger = require('./util/logger');
 const rExpAD = /^t{1}[0-9]{1}[a-zA-Z\d]+$/i
 const rExpPN = /^t{2}\d{7}$/i
 const rExpUPN = /^s{1}\d{7}$/i
@@ -37,9 +38,11 @@ module.exports = async (user) => {
   }
   else if (rExpPN.test(userId)) {
     user[fn.nv.personalNumber] = userId.substr(2)
-  } else {
-    user[fn.nv.personalNumber] = ""
   }
+  
+  if(user[fn.nv.personalNumber] == "" | user[fn.nv.personalNumber] == undefined) {
+     logger.error("user didnt get personal number from ad, inside getActiveDirectoryData module. user info: " + JSON.stringfy(user))
+   }
 
   return user;
 }
