27bddf29d70977f41226187e560749fdc6c4a9ec
Danie231 <danielezra10@gmail.com>
2019-01-06 17:00:02 +0200

    generate akaemployees

--- mocks/adfs.js
+++ mocks/adfs.js
@@ -4,21 +4,30 @@ const ae = require("./mocksFiles/getAkaEmployees.json")
 
 ga = [];
 
-for (let i = 0; i < 250; i++) {
+for (let i = 0; i < 150; i++) {
   bla = {}
   bla.KfirstName = faker.name.firstName();
   bla.KlastName = faker.name.lastName();
   bla.Kjob = faker.name.jobTitle();
   bla.mail = faker.internet.email();
-  if(ae[i].mi) {
-    bla.entity = "M" + ae[i].mi
-  } else {
-    bla.entity = "D" + ae[i].tz
-  }
+  bla.entity = "M" + ae[i].mi
   bla.hierarchy = faker.lorem.word() + "/" + faker.lorem.word() + "/" + faker.lorem.word() + "/" +
                     bla.Kjob + " - " + bla.KfirstName + " " + bla.KlastName;
 
   ga.push(bla)
 }
+
+for (let i = 0; i < 100; i++) {
+    bla = {}
+    bla.KfirstName = faker.name.firstName();
+    bla.KlastName = faker.name.lastName();
+    bla.Kjob = faker.name.jobTitle();
+    bla.mail = faker.internet.email();
+    bla.entity = "D" + faker.random.number();
+    bla.hierarchy = faker.lorem.word() + "/" + faker.lorem.word() + "/" + faker.lorem.word() + "/" +
+                      bla.Kjob + " - " + bla.KfirstName + " " + bla.KlastName;
+  
+    ga.push(bla)
+  }
 
 fs.writeFileSync("AD.json", JSON.stringify(ga))
--- mocks/faker-gen.js
+++ mocks/faker-gen.js
@@ -1,18 +1,17 @@
 const faker = require('faker')
 const ad = require("./mocksFiles/AD.json")
-const ae = require("./mocksFiles/getAkaEmployees.json")
-const at = require("./mocksFiles/getAkaTelephone.json")
-const es = require("./mocksFiles/eightsocks.json")
+const ae = require("./getAkaEmployees.json")
+const at = require("./getAkaTelephone.json")
+// const es = require("./mocksFiles/eightsocks.json")
 const fs = require("fs")
 let adUsers = [];
-console.log(at.length + " " + ad.length + " " +ae.length + " " +es.length)
-for (let i = 0; i < ad.length; i++) {
-  at[i].mi = ad[i].userPrincipalName;
-  ae[i].mi = ad[i].userPrincipalName;
-  es[i].tz = ae[i].tz;
-  es[i].mi = ae[i].mi;
+for (let i = 0; i < 250; i++) {
+  for (let j = 0; j < 250; j++) {
+    if(ae[j].mi == ad[i].entity.slice('M')[1]) {
+      ae[j].firstName = ad[i].KfirstName;
+      ae[j].lastName = ad[i].KlastName;
+    }
+  }
 }
 
-fs.writeFileSync("getAkaTelephone.json", JSON.stringify(at))
 fs.writeFileSync("getAkaEmployees.json", JSON.stringify(ae))
-fs.writeFileSync("eightsocks.json", JSON.stringify(es))
