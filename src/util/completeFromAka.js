fe1be2fc8c7c6da6464b5b66004c90f88f1a560f
weizmm <weizmm05@gmail.com>
2019-01-08 13:09:29 +0200

    start working on completeFromAka for ads

--- src/util/completeFromAka.js
+++ src/util/completeFromAka.js
@@ -5,7 +5,7 @@ This module add fields from aka to given object.
 */
 
 const complete_nv = (obj, akaData, dataSource) => {
-    complete = false;
+    let complete = false;
     let akaRecord = akaData.find(person => person[fn.aka.personalNumber] === obj.personalNumber.toString());
     if (akaRecord) {
         complete = true;
@@ -24,7 +24,7 @@ const complete_nv = (obj, akaData, dataSource) => {
 };
 
 const complete_es = (obj, akaData, dataSource) => {
-    complete = false;
+    let complete = false;
     let akaRecord = akaData.find(person => person[fn.aka.identityCard] === obj.identityCard.toString());
     if (akaRecord) {
         complete = true;
@@ -33,7 +33,23 @@ const complete_es = (obj, akaData, dataSource) => {
     if (!complete) { logger.warn(`The person with the identityCard ${obj.identityCard} from ${dataSource} not complete from aka`) };
 };
 
-
+const complete_ads = (obj, akaData, dataSource) => {
+    let complete = false;
+    let akaRecord = akaData.find(person => person[fn.aka.personalNumber] === obj.personalNumber.toString());
+    if (akaRecord) {
+        complete = true;
+        obj.identityCard = akaRecord[fn.aka.identityCard];
+        obj.firstName = akaRecord[fn.aka.firstName];
+        obj.lastName = akaRecord[fn.aka.lastName];
+        obj.rank = akaRecord[fn.aka.rank];
+        akaRecord[fn.aka.phone] ? obj.phone = [`${akaRecord[fn.aka.areaCode]}-${akaRecord[fn.aka.phone]}`] : null;
+        akaRecord[fn.aka.mobilePhone] ? obj.mobilePhone = [`${akaRecord[fn.aka.areaCodeMobile]}-${akaRecord[fn.aka.mobilePhone]}`] : null;
+        obj.dischargeDay = akaRecord[fn.aka.dischargeDay];
+        obj.clearance = akaRecord[fn.aka.clearance];
+        obj.currentUnit = akaRecord[fn.aka.unitName];
+    }
+    if (!complete) { logger.warn(`The person with the identityCard ${obj.identityCard} from ${dataSource} not complete from aka`) };
+}
 
 
 module.exports = (obj, akaData, dataSource) => {
@@ -43,6 +59,10 @@ module.exports = (obj, akaData, dataSource) => {
             break;
         case "nv":
             complete_nv(obj, akaData, dataSource);
+            break;
+        case "ads":
+            if (obj.entityType === fn.entityTypeValue.c){break;};
+            complete_ads(obj, akaData, dataSource);
             break;
         default:
             logger.error(`'dataSource' variable must be attached to 'completeFromAka' function`);
