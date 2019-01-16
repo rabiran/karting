92f6019c7203d6db30e336acce8cf89ef6f14c28
weizmm <weizmm05@gmail.com>
2019-01-16 12:17:52 +0200

    match the user_object structure to upgraded kartoffel

--- src/util/diffsHandler.js
+++ src/util/diffsHandler.js
@@ -36,7 +36,7 @@ const added = async (diffsObj, dataSource, aka_all_data) => {
             .then((person) => {
                 let user_object = {
                     personId: person.data.id,
-                    fullString: person_ready_for_kartoffel.mail,
+                    uniqueId: person_ready_for_kartoffel.mail,
                     isPrimary: false,
                 };
                 axios.post(p().KARTOFFEL_DOMAIN_USER_API, user_object)
@@ -69,7 +69,7 @@ const added = async (diffsObj, dataSource, aka_all_data) => {
                             // add primary user to the new person
                             let user_object = {
                                 personId: person.data.id,
-                                fullString: person.data.mail,
+                                uniqueId: person.data.mail,
                                 isPrimary: true,
                             };                      
                             axios.post(p().KARTOFFEL_DOMAIN_USER_API, user_object)
