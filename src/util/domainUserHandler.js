6b5302fde56d045ee8981a79aeb4775831546be7
weizmm <weizmm05@gmail.com>
2019-02-07 13:24:59 +0200

    solve the problem of the people from es that not have mail field

--- src/util/domainUserHandler.js
+++ src/util/domainUserHandler.js
@@ -3,10 +3,10 @@ const p = require('../config/paths');
 const axios = require('axios');
 const logger = require('./logger');
 
-module.exports = async (person, person_ready_for_kartoffel,record, isPrimary, dataSource) => {
+module.exports = async (person, person_ready_for_kartoffel, record, isPrimary, dataSource) => {
     let user_object = {
         personId: person.data.id,
-        uniqueID: person_ready_for_kartoffel.mail,
+        uniqueID: person_ready_for_kartoffel.mail,//##############maybe cancel this field?
         isPrimary: isPrimary,
     };
 
@@ -15,16 +15,18 @@ module.exports = async (person, person_ready_for_kartoffel,record, isPrimary, da
     (dataSource === "es" && record[fn.es.userName]) ?
         user_object.uniqueID = `${record[fn.es.userName]}${fn.es.domainSuffix}` :
         logger.warn(`The user with the identifyer ${person.data.identityCard || person.data.personalNumber} from ${dataSource} does not have ${fn.es.userName} field`);
+    // #######################need to condition the post process by the completenes of the uniqueID field
+    if (user_object.uniqueID) {
+        try {
+            const user = await axios.post(p().KARTOFFEL_DOMAIN_USER_API, user_object);
+            (user.data.entityType == fn.entityTypeValue.s) ?
+                logger.info(`Create the ${(isPrimary) ? "primary" : "secondary"} user ${user_object.uniqueID} to the person with personalNumber: ${user.data.personalNumber} from ${dataSource}_complete_data successfully.`) :
+                logger.info(`Create the ${(isPrimary) ? "primary" : "secondary"} user ${user_object.uniqueID} to the person with identityCard: ${user.data.identityCard} from ${dataSource}_complete_data successfully.`);
 
-    try {
-        const user = await axios.post(p().KARTOFFEL_DOMAIN_USER_API, user_object);
-        (user.data.entityType == fn.entityTypeValue.s) ?
-            logger.info(`Create the ${(isPrimary) ? "primary" : "secondary"} user ${user_object.uniqueID} to the person with personalNumber: ${user.data.personalNumber} from ${dataSource}_complete_data successfully.`) :
-            logger.info(`Create the ${(isPrimary) ? "primary" : "secondary"} user ${user_object.uniqueID} to the person with identityCard: ${user.data.identityCard} from ${dataSource}_complete_data successfully.`);
-
-    } catch (err) {
-        (person.data.entityType == fn.entityTypeValue.s) ?
-            logger.error(`Not create ${(isPrimary) ? "primary" : "secondary"} user to person with the identifyer: ${person.data.mail} to the person with personalNumber: ${person.data.personalNumber} from ${dataSource}_complete_data. The error message:"${err.response.data}"`) :
-            logger.error(`Not create ${(isPrimary) ? "primary" : "secondary"} user to person with the identifyer: ${person.data.mail} to the person with identityCard: ${person.data.identityCard} from ${dataSource}_complete_data. The error message:"${err.response.data}"`);
+        } catch (err) {
+            (person.data.entityType == fn.entityTypeValue.s) ?
+                logger.error(`Not create ${(isPrimary) ? "primary" : "secondary"} user to person with the identifyer: ${person.data.mail} to the person with personalNumber: ${person.data.personalNumber} from ${dataSource}_complete_data. The error message:"${err.response.data}"`) :
+                logger.error(`Not create ${(isPrimary) ? "primary" : "secondary"} user to person with the identifyer: ${person.data.mail} to the person with identityCard: ${person.data.identityCard} from ${dataSource}_complete_data. The error message:"${err.response.data}"`);
+        }
     }
 } 
