d0c902813f5d11e44f1519a8a9d6b0e98deb7d5f
Danie231 <danielezra10@gmail.com>
2019-03-14 14:20:24 +0200

    add files and dir

---
+++ mocks/mocksServer.js
@@ -0,0 +1,25 @@
+let express = require("express")
+let app = express()
+let port = 3001
+
+app.get("/getEightSocks", (req, res) => {
+    res.json(require("./mocksFiles/eightsocks.json"))
+})
+
+app.get("/getAkaTelephone", (req, res) => {
+    res.json(require("./mocksFiles/getAkaTelephone.json"))
+})
+
+app.get("/getAkaEmployees", (req, res) => {
+    res.json(require("./mocksFiles/getAkaEmployees.json"))
+})
+
+app.get("/getAD/s", (req, res) => {
+    res.json(require("./mocksFiles/AD.json"))
+})
+
+app.get("/getAD/NN", (req, res) => {
+    res.json(require("./mocksFiles/AD.json"))
+})
+
+app.listen(port, () => console.log("mocksGenerator server run on port:" + port))
