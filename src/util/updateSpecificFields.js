28aded9020ab73081b1fd3a3ec72c4af0ee728fc
weizmm <weizmm05@gmail.com>
2019-05-07 17:01:00 +0300

    fix in accordance to cr

--- src/util/updateSpecificFields.js
+++ src/util/updateSpecificFields.js
@@ -7,23 +7,19 @@ const logger = require('./logger');
  * This module accept an array that contain DeepDiff objects and build from them object for the PUT request that send to Kartoffel
  * @param {*} deepDiffArray Array of DeepDiff objects 
  * @param {*} dataSource The source of the raw person object
+ * @param {*} person Person object from Kartoffel
  */
+
 const updateSpecificFields = (deepDiffArray, dataSource, person) => {
     deepDiffArray.map(async (deepDiffRecord) => {
         let objForUpdate = {};
-        switch (deepDiffRecord.kind) {
-            case "E":
-                console.log("E");
-                objForUpdate[deepDiffRecord.path.toString()] = deepDiffRecord.rhs;
-                break;
-            case "N":
-                console.log("N");
-                objForUpdate[deepDiffRecord.path.toString()] = deepDiffRecord.rhs;
-                break;
-            default:
-                logger.warn(`the deepDiff kind of the updated person is not recognized -"${JSON.stringify(deepDiffRecord)}"`);
-                break;
+        if (deepDiffRecord.kind == "N" || deepDiffRecord.kind == "E") {
+            objForUpdate[deepDiffRecord.path.toString()] = deepDiffRecord.rhs;
         }
+        else {
+            logger.warn(`the deepDiff kind of the updated person is not recognized -"${JSON.stringify(deepDiffRecord)}"`);
+        }
+
         objForUpdate = await matchToKartoffel(objForUpdate, dataSource);
 
         // Update the person object
