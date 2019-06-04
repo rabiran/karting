e384a8acd9f83b83e3186d04bed863ca1dd8c3fa
DavidHeymann <heymann.david@gmail.com>
2019-06-04 16:36:36 +0300

    fix change gitignore

--- src/config/authParams.js
+++ src/config/authParams.js
@@ -2,7 +2,7 @@ module.exports = {
     clientId: '1234',
     ClientSecret: 'abcde',
     tokenHost: 'https://127.0.0.1',
-    tokenPort: '43',
+    tokenPort: '443',
     tokenPath:'oauth/token',
     scope:['read', 'write'],
     audience: 'myProgram',
