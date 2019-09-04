506a242ff54910a864ec3b9884bd6779685fde33
weizmm <weizmm05@gmail.com>
2019-09-04 11:05:22 +0300

    Refactor data synchronize (#88)
    * unite all synchronization functions into one function
    
    * update the comments
    
    * fix for pr
    
    * uncomment the datasources

--- src/config/paths.js
+++ src/config/paths.js
@@ -7,12 +7,12 @@ module.exports = pathsHandler = (param) => {
         // Data source
         AKA_TELEPHONES_API: "http://localhost:3001/getAkaTelephone",
         AKA_EMPLOYEES_API: "http://localhost:3001/getAkaEmployees",
-        ES_API: 'http://localhost:3001/getEightSocks',
-        ADS_API: 'http://localhost:3001/getAD/s',
-        ADNN_API: 'http://localhost:3001/getAD/NN',
-        MM_API: 'http://localhost:3001/getNv/sql/mm',
-        LMN_API: 'http://localhost:3001/getNv/sql/lmn',
-        MDN_API: 'http://localhost:3001/getNv/sql/mdn',
+        [`${fn.dataSources.es}_API`]: 'http://localhost:3001/getEightSocks',
+        [`${fn.dataSources.ads}_API`]: 'http://localhost:3001/getAD/s',
+        [`${fn.dataSources.adNN}_API`]: 'http://localhost:3001/getAD/NN',
+        [`${fn.dataSources.mm}_API`]: 'http://localhost:3001/getNv/sql/mm',
+        [`${fn.dataSources.lmn}_API`]: 'http://localhost:3001/getNv/sql/lmn',
+        [`${fn.dataSources.mdn}_API`]: 'http://localhost:3001/getNv/sql/mdn',
 
         // Kartoffel - person
         KARTOFFEL_UPDATE_PERSON_API: `${kartoffelUrl}/api/persons/${param}`,
---
+++ src/util/data_synchronizeData.js
@@ -0,0 +1,29 @@
+const p = require('../config/paths');
+const fn = require('../config/fieldNames');
+const axios = require("axios");
+const saveAsFile = require('../util/saveAsFile');
+const dataComparison = require('../util/dataComparison');
+const akaDataManipulate = require('./akaDataManipulate');
+
+module.exports = async (dataSource) => {
+    let data;
+    if (dataSource === fn.dataSources.aka) {
+        // get the update data from the remote server
+        let aka_telephones_data = await axios.get(p().AKA_TELEPHONES_API);
+        let aka_employees_data = await axios.get(p().AKA_EMPLOYEES_API);
+        // editing the aka data and squishing it to one object
+        data = akaDataManipulate(aka_telephones_data.data, aka_employees_data.data);
+    }
+    // get the update data from the remote server
+    else {
+        data = await axios.get(p()[`${dataSource}_API`]);
+        data = data.data;
+    }
+    // save the new json as file in the server and get the name of the kast file
+    let previous_data_file_name = saveAsFile(data, `./data/${dataSource}`, `${dataSource}_raw_data`);
+    // get the diffs between the two last JSONs
+    dataDiff = dataComparison(data, `./data/${dataSource}/archive`, previous_data_file_name, fn[dataSource].uniqeFieldForDeepDiff);
+    dataSource === fn.dataSources.aka ? dataDiff.all = data : null;
+
+    return dataDiff;
+}
