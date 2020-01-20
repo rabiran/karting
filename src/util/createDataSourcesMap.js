34ba827594851928a8b1849bcd4d916d228ab4c8
weizmm <weizmm05@gmail.com>
2020-01-20 20:10:20 +0200

    (fix) critic changes

--- src/util/createDataSourcesMap.js
+++ src/util/createDataSourcesMap.js
@@ -14,11 +14,11 @@ const result = excelToJson({
 
 const dataSourcesMap = new Map();
 result.dataSources.map((obj) => {
-    obj[fn.dataSources.es] ? dataSourcesMap.set(obj[fn.dataSources.es].replace(new RegExp("\"", 'g')," "), fn.dataSources.es) : null;
-    obj[fn.dataSources.nn] ? dataSourcesMap.set(obj[fn.dataSources.nn].replace(new RegExp("\"", 'g')," "), fn.dataSources.nn) : null;
-    obj[fn.dataSources.nv] ? dataSourcesMap.set(obj[fn.dataSources.nv].replace(new RegExp("\"", 'g')," "), fn.dataSources.nv) : null;
-    obj[fn.dataSources.ads] ? dataSourcesMap.set(obj[fn.dataSources.ads].replace(new RegExp("\"", 'g')," "), fn.dataSources.ads) : null;
-    obj[fn.dataSources.city] ? dataSourcesMap.set(obj[fn.dataSources.city].replace(new RegExp("\"", 'g')," "), fn.dataSources.city) : null;
+    obj[fn.dataSources.es] ? dataSourcesMap.set(obj[fn.dataSources.es].toString().replace(new RegExp("\"", 'g')," "), fn.dataSources.es) : null;
+    obj[fn.dataSources.nn] ? dataSourcesMap.set(obj[fn.dataSources.nn].toString().replace(new RegExp("\"", 'g')," "), fn.dataSources.nn) : null;
+    obj[fn.dataSources.nv] ? dataSourcesMap.set(obj[fn.dataSources.nv].toString().replace(new RegExp("\"", 'g')," "), fn.dataSources.nv) : null;
+    obj[fn.dataSources.ads] ? dataSourcesMap.set(obj[fn.dataSources.ads].toString().replace(new RegExp("\"", 'g')," "), fn.dataSources.ads) : null;
+    obj[fn.dataSources.city] ? dataSourcesMap.set(obj[fn.dataSources.city].toString().replace(new RegExp("\"", 'g')," "), fn.dataSources.city) : null;
 })
 
 module.exports = dataSourcesMap;
