96a0b9b12f27c0a77c81ca19c01f5fbb541c2a3d
Eli-BB8 <47026823+Eli-BB8@users.noreply.github.com>
2020-02-18 10:41:01 +0200

    Tags recovery (#113)
    * (feat) filter by tags
    
    * (feat) fiter city members by tags
    
    * (feat) filters per data source

--- src/util/recoveryUtils/recovery.js
+++ src/util/recoveryUtils/recovery.js
@@ -3,13 +3,16 @@ const getRawData = require('../getRawData');
 const compareToKartoffel = require('./compareToKartoffel');
 const matchToKartoffel = require('../matchToKartoffel');
 const fn = require('../../config/fieldNames');
+const recordsFilter = require('../recordsFilter');
 
 module.exports = async (akaData, dataSource) => {
     const dataFromDataSource = await getRawData(dataSource, fn.runnigTypes.recoveryRun, moment(new Date()).format("DD.MM.YYYY__HH.mm"));
 
+    const filteredData = recordsFilter(dataFromDataSource, dataSource);
+
     let adjustData = [];
-    for (let i = 0; i < dataFromDataSource.length; i++) {
-        adjustData.push(await matchToKartoffel(dataFromDataSource[i], dataSource));
+    for (let i = 0; i < filteredData.length; i++) {
+        adjustData.push(await matchToKartoffel(filteredData[i], dataSource));
     }
 
     await compareToKartoffel(adjustData, dataSource, akaData);
