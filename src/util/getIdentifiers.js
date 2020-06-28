b0792cee9343f4eff85be774681966dfff04e2b4
Eli-BB8 <elikovner1@gmail.com>
2020-06-28 11:14:29 +0300

    (fix) add assembleDomainUser in getIdentifiers

--- src/util/getIdentifiers.js
+++ src/util/getIdentifiers.js
@@ -1,5 +1,6 @@
 const matchToKartoffel = require('./matchToKartoffel');
 const fn = require('../config/fieldNames');
+const assembleDomainUser = require('./fieldsUtils/assembleDomainUser');
 
 /**
  * Returns the identifiers for the raw data record,
@@ -22,16 +23,7 @@ const getIdentifiers = async (record, dataSource) => {
             identityCard: matchedRecord.identityCard
         };
     } else if (matchedRecord.entityType === fn.entityTypeValue.gu) {
-        (dataSource === fn.dataSources.ads && record[fn[dataSource].sAMAccountName]) ?
-            domainUser = `${record[fn[dataSource].sAMAccountName]}${fn[dataSource].domainSuffix}` : null;
-        (dataSource === fn.dataSources.adNN && record[fn[dataSource].sAMAccountName]) ?
-            domainUser = `${record[fn[dataSource].sAMAccountName]}${fn[dataSource].domainSuffix}` : null;
-        (((dataSource === fn.dataSources.mdn) || (dataSource === fn.dataSources.lmn) || (dataSource === fn.dataSources.mm)) && record[fn[dataSource].uniqueID]) ?
-            domainUser = record[fn[dataSource].uniqueID].toLowerCase() : null;
-        (dataSource === fn.dataSources.es && record[fn[dataSource].userName]) ?
-            domainUser = `${record[fn[dataSource].userName]}${fn[dataSource].domainSuffix}` : null;
-        (dataSource === fn.dataSources.city && record[fn[dataSource].domainUsers]) ?
-            domainUser = `${record[fn[dataSource].domainUsers].toLowerCase()}` : null; 
+        domainUser = assembleDomainUser(dataSource, record);
     }
     
 
