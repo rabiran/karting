cfdcbf2903d29fafaf12ff3bb95d47b972e44caa
NoamYes <noamiko600@gmail.com>
2020-07-26 11:52:27 +0300

    Fix

--- src/util/diffsHandlerUtils/updatedDataHandler.js
+++ src/util/diffsHandlerUtils/updatedDataHandler.js
@@ -9,7 +9,6 @@ const updateSpecificFields = require('../updateSpecificFields');
 const recordsFilter = require('../recordsFilter');
 const tryArgs = require('../generalUtils/tryArgs');
 const getIdentifiers = require('../getIdentifiers')
-dataSource
 require('dotenv').config();
 
 /**
