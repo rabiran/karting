8bba958219cb19bfac99ea1ea2b154992484f770
NoamYes <noamiko600@gmail.com>
2020-08-05 17:31:13 +0300

    Apply fixes searchRecords

--- src/util/searchRecords.js
+++ src/util/searchRecords.js
@@ -2,32 +2,23 @@ const fn = require('../config/fieldNames');
 const getDataSourceFromFile = require('./getDataSourceFromFile');
 
 module.exports = async (identifiersArray, dataSources) => {
-
-    let sourceResults = [];
-    for (let dataSource of dataSources) {
-        sourceData = await getDataSourceFromFile(dataSource);
-        try {
-            let results = sourceData.reduce((res, record) => {
-                const foundID = identifiersArray.find(id => {
-                    return record[fn[dataSource].personalNumber] == id ||
-                    record[fn[dataSource].identityCard] == id;
-                })
-                if(foundID) {
-                    res.push({id: foundID, record: record});
-                }
-                return res;
-            }, [])
-            sourceResults.push({"dataSource": dataSource, "results": results})
-        } catch (err) {
-            console.log(err);
+  const sourceResults = {};
+  for (const dataSource of dataSources) {
+    const sourceData = await getDataSourceFromFile(dataSource);
+    try {
+      const results = sourceData.reduce((res, record) => {
+        const foundID = identifiersArray.find((id) => record[fn[dataSource].personalNumber] == id
+                    || record[fn[dataSource].identityCard] == id);
+        if (foundID) {
+          res.push({ id: foundID, record });
         }
+        return res;
+      }, []);
+      sourceResults[dataSource] = results;
+    } catch (err) {
+      console.log(err);
     }
-    return sourceResults;
+  }
+  return sourceResults;
+};
 
-}
-
-
-// async function findrecord(record, flatIDs, dataSource, Auth, sendLog) {
-//     const { identityCard, personalNumber, domainUser } = await getIdentifiers(record, dataSource, Auth, sendLog);
-//     return (flatIDs.includes(identityCard) || flatIDs.includes(personalNumber) || flatIDs.includes(domainUser));
-// }
