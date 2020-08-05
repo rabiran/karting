7e1d807e2c4688a2a29e2cec05be4f24d764b6a9
NoamYes <noamiko600@gmail.com>
2020-08-05 17:34:52 +0300

    Apply eslint fixes immediate

--- src/immediate.js
+++ src/immediate.js
@@ -2,34 +2,31 @@ const fn = require('./config/fieldNames');
 const diffsHandler = require('./util/diffsHandler');
 const logDetails = require('./util/logDetails');
 const preRun = require('./util/preRun');
-const searchRecords = require("./util/searchRecords");
+const searchRecords = require('./util/searchRecords');
 const AuthClass = require('./auth/auth');
 
+const { logLevel } = require('./util/logger');
 
-let { logLevel } = require('./util/logger');
 module.exports = async (dataSource, identifiersArray, runUID) => {
-    for (let identifierObj of identifiersArray) {
-        try {
-            let { dataObj, sendLog } = await preRun(fn.runnigTypes.immediateRun, 
-                [fn.dataSources.aka, dataSource]
-            ,identifierObj, runUID)
-            
-            let akaRecords = dataObj[fn.dataSources.aka].data;
-            let foundRecords = dataObj[dataSource].data;
+  for (const identifierObj of identifiersArray) {
+    try {
+      const { dataObj, sendLog } = await preRun(fn.runnigTypes.immediateRun,
+        [fn.dataSources.aka, dataSource],
+        identifierObj, runUID);
 
-            if(!foundRecords.length) {
-                let sourceResults = await searchRecords([identifierObj.identityCard, identifierObj.personalNumber], [dataSource])
-                foundRecords = [sourceResults[0].results[0].record];
-            }
+      const akaRecords = dataObj[fn.dataSources.aka].data;
+      let foundRecords = dataObj[dataSource].data;
 
-            let Auth = new AuthClass(sendLog);
+      if (!foundRecords.length) {
+        const sourceResults = await searchRecords([identifierObj.identityCard, identifierObj.personalNumber], [dataSource]);
+        foundRecords = sourceResults[dataSource];
+      }
 
-            await diffsHandler({ added: foundRecords }, dataSource, akaRecords, fn.runnigTypes.immediateRun, sendLog, Auth);
+      const Auth = new AuthClass(sendLog);
 
-
-        } catch (err) {
-            sendLog(logLevel.error, logDetails.error.ERR_UN_HANDLED_ERROR, fn.runnigTypes.immediateRun, JSON.stringify(err));
-        }
+      await diffsHandler({ added: foundRecords }, dataSource, akaRecords, fn.runnigTypes.immediateRun, sendLog, Auth);
+    } catch (err) {
+      sendLog(logLevel.error, logDetails.error.ERR_UN_HANDLED_ERROR, fn.runnigTypes.immediateRun, JSON.stringify(err));
     }
-
-}
+  }
+};
