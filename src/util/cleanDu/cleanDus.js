67e5d3ed49e9d8afa19f7bfbeeb7ba4714b2bd58
Eli-BB8 <elikovner1@gmail.com>
2020-09-24 11:18:58 +0300

    (fix) comparimg aganist kartoffel

--- src/util/cleanDu/cleanDus.js
+++ src/util/cleanDu/cleanDus.js
@@ -1,7 +1,7 @@
 const getIrrelevantDus = require('./getIrrelevantDus')
 
 async function cleanDu(dataSource, data, sendLog, Auth) {
-    const irrelevantDus = getIrrelevantDus(data, dataSource, sendLog);
+    const irrelevantDus = getIrrelevantDus(data, dataSource, sendLog, Auth);
     await deleteDus(irrelevantDus);
 }
 
--- src/util/cleanDu/getIrrelevantDus.js
+++ src/util/cleanDu/getIrrelevantDus.js
@@ -1,9 +1,9 @@
-const isRecordRelevant = require('./isRecordRelevant');
 const assembleDomainUser = require('../fieldsUtils/assembleDomainUser');
 const fn = require('../../config/fieldNames');
 const diff = require('diff-arrays-of-objects');
 const fs = require("fs");
 const { filename } = require('winston-daily-rotate-file');
+const p = require('../../config/paths');
 
 /**
  * check each record if its irrelevant anymore
@@ -14,114 +14,107 @@ const { filename } = require('winston-daily-rotate-file');
  * @param {string} dataSource
  * @param {Function} sendLog - logger
  */
-function getIrrelevantDus(data, dataSource, sendLog) {
-    const irrelevantRecords = filterIrrelevantByDataSource(dataSource, data)
-    const irrelevantDus = irrelevantRecords.map((record, dataSource) => isolateDu(dataSource, record, sendLog));
+function getIrrelevantDus(data, dataSource, sendLog, Auth) {
+    
+    
+    function isolateDu(dataSource, record, sendLog) {
+        return assembleDomainUser(dataSource, record, sendLog);
+    }
+    
+    /**
+     * filter the unneseccary records from accorrding to
+     * the current data
+     * 
+     * @param {string} dataSource - the data source
+     * @param {Array} data - all the current data from the data source
+     * @returns {Array} - all the irrelevant records
+     */
+    function filterIrrelevantByDataSource(dataSource, data) {
+        switch (dataSource) {
+            case fn.dataSources.aka:
+                return []
+            case fn.dataSources.es:
+                return getRemovedDu(data, dataSource);
+            case fn.dataSources.city:
+                return getRemovedDu(data, dataSource);
+            case fn.dataSources.ads:
+                return filterAdsRecord(data)
+            case fn.dataSources.adNN:
+                return filterAdNNRecord(data)
+            default:
+                sendLog(logLevel.error, logDetails.error.ERR_UNIDENTIFIED_DATA_SOURCE);
+        }
+    }
+                            
 
+    /**
+     * filter irelevance records for ads
+     *
+     * @param {Object} records 
+     * @returns {array} the records for delete
+     */
+    function filterAdsRecord(records) {
+        const irrelevattRecords = records.filter(record => !record[fn.ads_name.upn]);
+        const irrelevantDus = irrelevattRecords.map(record => isolateDu(dataSource, record, sendLog)
+    }
+    
+    /**
+     * filter irelevance records for adNN
+     * 
+     * @param {Object} records
+     * @returns {array} the records for delete
+     */
+    function filterAdNNRecord(records) {
+        return records.filter(record => !record[fn.adNN_name.upn])
+    }
+    
+    /**
+     * filter irrelevant records of dataSource
+     * 
+     * @param {Object} records - current data
+     * @param {string} dataSource - the data source of the data
+     * @returns {array} the records to delete
+     */
+    async function getRemovedDu(records, dataSource) {
+        const duToRemove = [];
+        const query = {
+            params: `domainUsers.dataSource=${dataSource}`
+        }
+
+        const dataSourcePersons = await Auth.axiosKartoffel.get(p().KARTOFFEL_PERSON_API, query);
+        
+        for (person of dataSourcePersons) {
+            for (domainUserObj of person.domainUsers) {
+                if (domainUserObj.dataSource !== dataSource) {
+                    continue;
+                }
+                
+                const parallelRecord = findParallelRecord(records, domainUserObj, dataSource);
+                
+                if (!parallelRecord) {
+                    duToRemove.push(domainUserObj.uniqeID);
+                }
+            }
+        }
+        
+        return duToRemove;
+    }
+    
+    /**
+     * 
+     * @param {Array} records - the records from dataSource
+     * @param {Object} domainUserObj - domainUser object from kartoffel
+     * @param {string} dataSource - the data source
+     * @returns {Object} - the parallel record of the domainUser
+     */
+    function findParallelRecord(records, domainUserObj, dataSource) {
+        //TODO...
+    }
+    
+    const irrelevantRecords = filterIrrelevantByDataSource(dataSource, data, sendLog, Auth)
+    const irrelevantDus = irrelevantRecords.map((record, dataSource) => isolateDu(dataSource, record, sendLog)); //no needl
+    
     return irrelevantDus;
-}
-
-function isolateDu(dataSource, record, sendLog) {
-    return assembleDomainUser(dataSource, record, sendLog);
-}
-
-/**
- * filter the unneseccary records from accorrding to
- * the current data
- * 
- * @param {string} dataSource - the data source
- * @param {Array} data - all the current data from the data source
- * @returns {Array} - all the irrelevant records
- */
-function filterIrrelevantByDataSource(dataSource, data) {
-    switch (dataSource) {
-        case fn.dataSources.aka:
-            return []
-        case fn.dataSources.es:
-            return filterEsRecord(data)
-        case fn.dataSources.ads:
-            return filterAdsRecord(data)
-        case fn.dataSources.adNN:
-            return filterAdNNRecord(data)
-        case fn.dataSources.mdn:
-        case fn.dataSources.mm:
-        case fn.dataSources.lmn:
-        case fn.dataSources.city:
-            return filterCityRecord(data)
-        default:
-            sendLog(logLevel.error, logDetails.error.ERR_UNIDENTIFIED_DATA_SOURCE);
-    }
-
-}
-
-
-/**
- * filter irelevance records for ads
- * 
- * @param {Object} records 
- * @returns {array} the records for delete
- */
-function filterAdsRecord(records) {
-    return records.filter(record => !record[fn.ads_name.upn])
-}
-
-/**
- * filter irelevance records for adNN
- * 
- * @param {Object} records
- * @returns {array} the records for delete
- */
-function filterAdNNRecord(records) {
-    return records.filter(record => !record[fn.adNN_name.upn])
-}
-
-/**
- * filter irelevance records for es
- * 
- * @param {Object} records 
- * @returns {array} the records for delete
- */
-function filterEsRecord(records) {
-    let files = fs.readdirSync(`${fn.esRecoveryDataFolder}`);
-    let filename = files.find(file => file.endsWith('.log'))
-    let esData = JSON.parse(fs.readFileSync(`${fn.esRecoveryDataFolder}${filename}`));
-
-    let diffObj = diff(esData, records);
-    
-    let recordsToRemove = diffObj.removed;
-    for (const updatdRecord of updatedRcordsIds) {
-        let oldRecord = esData.find(record => record[fn[dataSources.es_name].mail] == record[fn[dataSources.es_name].mail])
-        if (
-            oldRecord[fn[dataSources.es_name].identityCard] != updatdRecord[fn[dataSources.es_name].identityCard] || 
-            oldRecord[fn[dataSources.es_name].personalNumber] != updatdRecord[fn[dataSources.es_name].personalNumber]
-        ) {
-            recordsToRemove.push(oldRecord);
-        }
-    }
-    return recordsToRemove;
-}
-
-/**
- * filter irelevance records for city
- * 
- * @param {Object} records 
- * @returns {array} the records for delete
- */
-function filterCityRecord(records) {
-    let files = fs.readdirSync(`${fn.cityRecoveryDataFolder}`);
-    let filename = files.find(file => file.endsWith('.log'))
-    let cityData = JSON.parse(fs.readFileSync(`${fn.cityRecoveryDataFolder}${filename}`));
-
-    let diffObj = diff(cityData, records);
-    
-    let recordsToRemove = diffObj.removed;
-    for (const updatdRecord of updatedRcordsIds) {
-        let oldRecord = cityData.find(record => record[fn[dataSources.es_name].mail] == record[fn[dataSources.es_name].mail])
-        if (oldRecord[fn[dataSources.city].identityCard] != updatdRecord[fn[dataSources.city].identityCard]) {
-            recordsToRemove.push(oldRecord);
-        }
-    }
-    return recordsToRemove;
 }
 
 module.exports = getIrrelevantDus;
