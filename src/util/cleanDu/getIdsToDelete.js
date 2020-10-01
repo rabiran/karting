f1b6013c8030901aa3c8626c195b75d133d8ab90
hagai245 <chagaimillot@gmail.com>
2020-10-01 11:37:32 +0300

    deleted getIdsToDelete file

--- src/util/cleanDu/getIdsToDelete.js
+++
@@ -1,9 +0,0 @@
-const fn = require('../../config/fieldNames')
-const assembleDomainUser = require('../fieldsUtils/assembleDomainUser')
-
-module.exports = (dataSource, sourceRecordsList, kartoffelPersonList, sendLog) =>{
-    const sourceRecordsDUs = sourceRecordsList.map(record => assembleDomainUser(dataSource, record, sendLog));
-    const kartoffelPersonsDUs = kartoffelPersonList.map(person => person.domainUsers.filter(obj => obj.dataSource == dataSource)).flat().map(obj => obj.uniqueID);
-    const idsToRemove = kartoffelPersonsDUs.filter(x => !sourceRecordsDUs.includes(x));
-    return idsToRemove;
-}
