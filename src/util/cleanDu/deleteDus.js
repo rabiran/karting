12d968bbe0f12a04c4dca242cb6be39a616c7196
hagai245 <chagaimillot@gmail.com>
2020-10-12 14:09:19 +0300

    another fix

--- src/util/cleanDu/deleteDus.js
+++ src/util/cleanDu/deleteDus.js
@@ -11,7 +11,7 @@ module.exports = async (domainUsers, dataSourcePersons, dataSource, sendLog, Aut
             let id = person._id;
             try{
                 await Auth.axiosKartoffel.delete(p(id , domainUser).KARTOFFEL_DELETE_DOMAIN_USER_API);
-                sendLog(logLevel.info, logDetails.info.INF_DELETE_DOMAIN_USER, domainUser, dataSource, person.personalNumber || person.identityCard)
+                sendLog(logLevel.info, logDetails.info.INF_DELETE_DOMAIN_USER, domainUser, person.personalNumber || person.identityCard, dataSource)
             } catch (err){
                 sendLog(logLevel.error, logDetails.error.ERR_DELETE_DOMAIN_USER , domainUser, id, dataSource, err.message);
             }
