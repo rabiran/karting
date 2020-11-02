69ac2075df6a25efa0869c071f9a160e9c7dd340
NoamYes <noamiko600@gmail.com>
2020-11-02 12:03:20 +0200

    Change removeDu function for runningTypes

--- src/util/cleanDu/cleanDus.js
+++ src/util/cleanDu/cleanDus.js
@@ -6,15 +6,24 @@ const logDetails = require('../logDetails');
 const p =  require('../../config/paths');
 const AuthClass = require('../../auth/auth');
 
-async function cleanDu(dataSource, records, query, sendLog, Auth) {
-    if(dataSource != fn.dataSources.aka) {
-        const dataSourcePersons = await Auth.axiosKartoffel.get(p().KARTOFFEL_PERSON_API, query).catch(err => {
-                sendLog(logLevel.error, logDetails.error.ERR_GET_PERSONS_BY_DU , dataSource, err.message);
-            });
-        
-        const irrelevantDus = getIrrelevantDus(records, dataSourcePersons.data, dataSource, sendLog, Auth);
-        await deleteDus(irrelevantDus, dataSourcePersons.data, dataSource, sendLog, Auth);
+async function cleanDu(runningType, dataSource, records, query, sendLog, Auth) {
+
+    let dataSourcePersons = [];
+    if(runningType === fn.runnigTypes.immediateRun) {
+        const resPerson = await Auth.axiosKartoffel.get(`${p().KARTOFFEL_PERSON_API}/domainUser/${query}`).catch(err => {
+            sendLog(logLevel.error, logDetails.error.ERR_GET_PERSONS_BY_DU , dataSource, err.message);
+        });
+        dataSourcePersons.push(resPerson.data);
     }
+    else {
+        const resPersons = await Auth.axiosKartoffel.get(p().KARTOFFEL_PERSON_API, query).catch(err => {
+            sendLog(logLevel.error, logDetails.error.ERR_GET_PERSONS_BY_DU , dataSource, err.message);
+        });
+        dataSourcePersons = resPersons.data;
+    }
+    const irrelevantDus = getIrrelevantDus(records, dataSourcePersons, dataSource, sendLog, Auth);
+    await deleteDus(irrelevantDus, dataSourcePersons.data, dataSource, sendLog, Auth);
+    
 }
 
 module.exports = cleanDu;
