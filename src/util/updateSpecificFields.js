47b73948dff794be3dac1173cab9af4fad951ba0
NoamYes <noamiko600@gmail.com>
2020-12-24 15:50:59 +0200

    Change isMatch to needMatch

--- src/util/updateSpecificFields.js
+++ src/util/updateSpecificFields.js
@@ -59,7 +59,7 @@ const updateSpecificFields = async (DataModel) => {
     });
     // when person from 'diffsHandler-added' come to update they already passed through 'matchToKartoffel'
     // and if the them sending again to 'matchToKartoffel' the keys of the object will be deleted
-    if (DataModel.isMatchToKartoffel) {
+    if (DataModel.needMatchToKartoffel) {
         objForUpdate = await matchToKartoffel(objForUpdate, DataModel.dataSource, DataModel.Auth, DataModel.sendLog, fn.flowTypes.update);
     }
 
